dafe32318651daa297d6280eeeb75b4c0ee30950
dianlujitao <dianlujitao@lineageos.org>
2024-04-12 21:30:18 +0700

    sdm845-common: Drop Snap
    Change-Id: Ie5e2c3d337a3d80795d6d307eb5cc4326d73bbd0

--- sdm845.mk
+++ sdm845.mk
@@ -148,7 +148,6 @@ TARGET_SCREEN_WIDTH := 1440
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Component overrides
